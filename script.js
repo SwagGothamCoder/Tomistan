b13a53931c8539a8893879f79b4c0d55d7827cca
SwagGothamCoder <skiingrunner5879@gmail.com>
2016-05-18 16:21:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 var textElement = document.getElementById("day");
+if (textElement.innerText) {
+    textElement.innerText = "newtext";
+}
+else
+if (textElement.textContent) {
+    textElement.textContent = "newtext";   
+}
